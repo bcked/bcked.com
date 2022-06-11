a4bc61b11fe92781ae3059889952e5dd623b68c4
Spenhouet <Spenhouet@users.noreply.github.com>
2022-06-11 22:40:47 +0000

    Deploying to gh-pages from @ Spenhouet/backed@34a92031e0e9b2756952f43b4edf855e18e32af7 🚀

---
+++ _app/immutable/pages/token/_address_.svelte-fe562f10.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as a}from"../../chunks/index-c9c51329.js";async function o(){return{status:302,redirect:"/"}}class u extends t{constructor(s){super(),e(this,s,null,null,a,{})}}export{u as default,o as load};
