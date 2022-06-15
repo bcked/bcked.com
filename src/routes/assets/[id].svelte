6c4e84406e6e936e6a2e19c95bbb37e820b815de
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-16 00:45:34 +0200

    Use card

--- src/routes/assets/[id].svelte
+++ src/routes/assets/[id].svelte
@@ -8,14 +8,19 @@
 	let data;
 </script>
 
-<p>{$page.params.id}</p>
-
-<div class="flex justify-center">
-	<div class="w-full h-96">
-		<LayerCake {data}>
-			<Svg>
-				<Sankey colorNodes={(d) => '#00bbff'} colorText={(d) => '#ffffff'} />
-			</Svg>
-		</LayerCake>
+<div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 my-10">
+	<div class="px-4 py-5 sm:px-6">
+		<div>{$page.params.id}</div>
+	</div>
+	<div class="px-4 py-5 sm:p-6">
+		<div class="flex justify-center">
+			<div class="w-full h-96">
+				<LayerCake {data}>
+					<Svg>
+						<Sankey colorNodes={(d) => '#00bbff'} colorText={(d) => '#ffffff'} />
+					</Svg>
+				</LayerCake>
+			</div>
+		</div>
 	</div>
 </div>
