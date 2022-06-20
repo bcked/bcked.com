76d008cd87f69477a9e26e1c44013a946bd3e360
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-20 20:19:15 +0200

    Remove background 404 page

--- src/lib/components/404.svelte
+++ src/lib/components/404.svelte
@@ -32,7 +32,7 @@
 	</div>
 </div> -->
 
-<div class="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+<div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
 	<div class="max-w-max mx-auto">
 		<main class="sm:flex">
 			<p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
