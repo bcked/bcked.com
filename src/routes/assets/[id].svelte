a797fa76c3ee9f38a60cadbf74463fef2540ec2a
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-20 20:18:32 +0200

    Add asset details

--- src/routes/assets/[id].svelte
+++ src/routes/assets/[id].svelte
@@ -11,14 +11,27 @@
 </script>
 
 <header>
-	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+	<div class="flex space-x-2 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 		<h1 class="text-3xl font-bold leading-tight text-gray-900">{asset.name}</h1>
+		{#if asset.symbol}
+			<span
+				class="inline-flex items-center my-auto px-2.5 py-0.5 rounded-md text-sm font-medium bg-gray-200 text-gray-800"
+			>
+				{asset.symbol}
+			</span>
+		{/if}
 	</div>
 </header>
 <main>
 	<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+		{#if asset.description}
+			<span>
+				{asset.description}
+			</span>
+		{/if}
+
 		<div class="flex justify-center">
-			<div class="w-full h-96">
+			<div class="py-4 w-full h-96">
 				<LayerCake data={data.tree}>
 					<Svg>
 						<Sankey colorNodes={(d) => '#00bbff'} colorText={(d) => '#ffffff'} />
