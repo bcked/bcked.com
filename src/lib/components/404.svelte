910e2ed9b6f3c1b7a3d61baf47b02fedf8fdb4b5
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-23 22:48:23 +0200

    404 page theming

--- src/lib/components/404.svelte
+++ src/lib/components/404.svelte
@@ -35,7 +35,7 @@
 <div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
 	<div class="max-w-max mx-auto">
 		<main class="sm:flex">
-			<p class="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
+			<p class="text-4xl font-extrabold text-neon-pink sm:text-5xl">404</p>
 			<div class="sm:ml-6">
 				<div class="sm:border-l sm:border-gray-200 sm:pl-6">
 					<h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
@@ -48,13 +48,13 @@
 				<div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
 					<a
 						href={`${base}/`}
-						class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-neon-pink hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-neon-pink"
 					>
 						Go back home
 					</a>
 					<a
 						href="https://github.com/Spenhouet/backed/issues"
-						class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-gray-900 bg-neon-yellow hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-neon-yellow"
 					>
 						<GitHubIcon class="mr-3 text-opacity-50 transform" />
 						Open issue on GitHub
