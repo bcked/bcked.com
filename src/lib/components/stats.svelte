96d89f3fa2d1d4b3c1435ff0a12bfd410b4465b9
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-27 23:21:50 +0200

    Add stats panel

---
+++ src/lib/components/stats.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {Object[]} */
+	export let stats;
+
+	const columns = stats.length <= 4 ? stats.length : 4;
+</script>
+
+<div>
+	<dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-{columns}">
+		{#each stats as item}
+			<div key={item.name} class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+				<dt class="text-sm font-medium text-gray-500 truncate">{item.name}</dt>
+				<dd class="mt-1 text-3xl font-semibold text-gray-900">{item.stat}</dd>
+			</div>
+		{/each}
+	</dl>
+</div>
--- src/routes/assets/[id]/index.svelte
+++ src/routes/assets/[id]/index.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import Sankey from './sankey@sankey.svelte';
+	import Stats from '$lib/components/stats.svelte';
 	import Progress from '$lib/components/progress.svelte';
 
 	/** @type {any} */
@@ -26,37 +27,28 @@
 					{/if}
 				</div>
 			</div>
-			<div
-				class="mt-4 flex items-center sm:justify-between space-x-4 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:justify-start"
-			>
-				{#if asset.backing.ratio > 0}
-					<span
-						class="inline-flex items-center my-auto px-2.5 py-0.5 rounded-md text-lg font-medium bg-gray-200 text-gray-900"
-					>
-						Backing: {Math.round(asset.backing.ratio * 100)}%
-					</span>
-					<span
-						class="inline-flex items-center my-auto px-2.5 py-0.5 rounded-md text-lg font-medium bg-gray-200 text-gray-900"
-					>
-						Distribution: {Math.round(asset.backing.distribution * 100)}%
-					</span>
-				{:else}
-					<span
-						class="inline-flex items-center my-auto sm:px-2.5 py-0.5 rounded-md text-lg font-medium bg-red-300 text-gray-900"
-					>
-						Backing: None</span
-					>
-					<span
-						class="inline-flex items-center my-auto sm:px-2.5 py-0.5 rounded-md text-lg font-medium bg-red-300 text-gray-900"
-					>
-						Distribution: N/A
-					</span>
-				{/if}
-			</div>
 		</div>
 	</header>
 	<main>
-		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4">
+		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-4 space-y-4">
+			<Stats
+				stats={[
+					{
+						name: 'Price',
+						stat: `$${asset.price.usd}`
+					},
+					{
+						name: 'Backing Ratio',
+						stat: asset.backing.ratio > 0 ? `${Math.round(asset.backing.ratio * 100)}%` : 'None'
+					},
+					{
+						name: 'Backing Distribution',
+						stat:
+							asset.backing.ratio > 0 ? `${Math.round(asset.backing.distribution * 100)}%` : 'N/A'
+					}
+				]}
+			/>
+
 			<Progress
 				textLeft="Backing: ${asset.backing['backing-usd']}"
 				textRight="Market Cap: ${asset.mcap}"
