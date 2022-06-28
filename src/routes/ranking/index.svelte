4111e2df85546419c972fc853dc8093cea3b0376
Sebastian Penhouet <s.penhouet@outlook.de>
2022-06-28 23:34:39 +0200

    Add table placeholder

--- src/routes/ranking/index.svelte
+++ src/routes/ranking/index.svelte
@@ -1,3 +1,16 @@
+<script>
+	import { onMount } from 'svelte';
+	import { base } from '$app/paths';
+	import Table from '$lib/components/table.svelte';
+
+	/** @type {any} */
+	let assets = {};
+
+	onMount(async function () {
+		assets = await (await fetch(`${base}/assets.json`)).json();
+	});
+</script>
+
 <div class="py-10">
 	<header>
 		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -8,5 +21,6 @@
 		<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 			<p>Compare the backing of different assets.</p>
 		</div>
+		<Table />
 	</main>
 </div>
