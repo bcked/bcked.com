4d99609c34290a3f43026bde5ad896b72a569e1f
Sebastian Penhouet <s.penhouet@outlook.de>
2022-07-02 23:26:04 +0200

    Fix landing path

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,9 @@
 <script>
+	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	onMount(async function () {
-		goto('/ranking');
+		goto(`${base}/ranking`);
 	});
 </script>
 
