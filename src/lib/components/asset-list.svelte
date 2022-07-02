b3ad205b9b220ec9561c44d2136332834fe1f1c5
Sebastian Penhouet <s.penhouet@outlook.de>
2022-07-02 23:21:08 +0200

    Extract asset list

---
+++ src/lib/components/asset-list.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { ChevronRightIcon } from '@rgossiaux/svelte-heroicons/solid';
+
+	/** @type {any} */
+	export let assets;
+	/** @type {any} */
+	export let icon;
+	/** @type {string} */
+	export let title;
+	/** @type {(a: any, b: any) => Number} */
+	export let compare;
+	/** @type {Number} */
+	export let size;
+	/** @type {(asset: any) => string} */
+	export let select;
+</script>
+
+<div key={title} class="px-4 py-5 overflow-hidden sm:p-6">
+	<div>
+		<div class="relative">
+			<!-- <div class="absolute -inset-0 flex items-center space-x-2 text-neon-yellow blur-sm">
+				<svelte:component this={icon} class="h-5 w-5" aria-hidden="true" />
+				<span class="text-lg font-semibold">{title}</span>
+			</div> -->
+			<div class="relative flex items-center space-x-2 ">
+				<svelte:component this={icon} class="h-5 w-5 text-neon-pink" aria-hidden="true" />
+				<span class="text-lg font-semibold">{title}</span>
+			</div>
+		</div>
+	</div>
+	<ul class="mt-2 space-y-2">
+		{#each Object.values(assets).sort(compare).slice(0, size) as { asset }, i}
+			<li key={asset.id}>
+				<a href={asset.path} class="rounded-lg block">
+					<div class="flex items-center min-w-0 flex-1 justify-between">
+						<div class="flex items-center">
+							<div class="text-gray-400 text-sm">
+								{i + 1}
+							</div>
+							<div class="flex items-center px-4 sm:px-6">
+								<div class="font-semibold">
+									{asset.name}
+								</div>
+								<div class="pl-1 text-gray-400 text-sm">
+									{asset.symbol}
+								</div>
+							</div>
+						</div>
+						<div class="flex items-center">
+							<div>
+								{select(asset)}
+							</div>
+							<div class="ml-5 flex-shrink-0">
+								<ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+							</div>
+						</div>
+					</div>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/assets/index.svelte
+++ src/routes/assets/index.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { ChevronRightIcon } from '@rgossiaux/svelte-heroicons/solid';
+	import { ChartBarIcon, ClockIcon, HashtagIcon } from '@rgossiaux/svelte-heroicons/outline';
 	import { formatCurrency } from '$lib/utils/string-formatting';
+	import AssetList from '$lib/components/asset-list.svelte';
 
 	/** @type {any} */
 	export let assets;
@@ -25,114 +26,31 @@
 			<dl
 				class="rounded-lg overflow-hidden shadow mt-5 grid grid-cols-1  bg-white divide-y divide-gray-200 lg:divide-x lg:divide-y-0 lg:gap-px lg:grid-cols-3"
 			>
-				<div key="backing-usd" class="px-4 py-5 overflow-hidden sm:p-6">
-					<span class="text-lg font-semibold">Backing</span>
-					<ul role="list" class="mt-2">
-						{#each Object.values(assets)
-							.sort((a, b) => b.asset.backing['backing-usd'] - a.asset.backing['backing-usd'])
-							.slice(0, 3) as { asset }, i}
-							<li key={asset.id}>
-								<a href={asset.path} class="rounded-lg block hover:bg-gray-50">
-									<div class="flex items-center min-w-0 flex-1 justify-between">
-										<div class="flex items-center">
-											<div class="text-gray-600 text-sm">
-												{i + 1}
-											</div>
-											<div class="flex items-center px-4 sm:px-6">
-												<div class="font-semibold">
-													{asset.name}
-												</div>
-												<div class="pl-1 text-gray-600">
-													{asset.symbol}
-												</div>
-											</div>
-										</div>
-										<div class="flex items-center">
-											<div>
-												{formatCurrency(asset.backing['backing-usd'])}
-											</div>
-											<div class="ml-5 flex-shrink-0">
-												<ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-											</div>
-										</div>
-									</div>
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
-				<div key="backing-assets" class="px-4 py-5 overflow-hidden sm:p-6">
-					<span class="text-lg font-semibold">Backing Assets</span>
-					<ul role="list" class="mt-2">
-						{#each Object.values(assets)
-							.sort((a, b) => b.asset.backing['backing-assets'] - a.asset.backing['backing-assets'])
-							.slice(0, 3) as { asset }, i}
-							<li key={asset.id}>
-								<a href={asset.path} class="rounded-lg block hover:bg-gray-50">
-									<div class="flex items-center min-w-0 flex-1 justify-between">
-										<div class="flex items-center">
-											<div class="text-gray-600 text-sm">
-												{i + 1}
-											</div>
-											<div class="flex items-center px-4 sm:px-6">
-												<div class="font-semibold">
-													{asset.name}
-												</div>
-												<div class="pl-1 text-gray-600">
-													{asset.symbol}
-												</div>
-											</div>
-										</div>
-										<div class="flex items-center">
-											<div>
-												{asset.backing['backing-assets']}
-											</div>
-											<div class="ml-5 flex-shrink-0">
-												<ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-											</div>
-										</div>
-									</div>
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
-				<div key="Recently Updated" class="px-4 py-5 overflow-hidden sm:p-6">
-					<span class="text-lg font-semibold">Recently Updated</span>
-					<ul role="list" class="mt-2">
-						{#each Object.values(assets)
-							.sort((a, b) => new Date(a.asset.backing['updated']) - new Date(b.asset.backing['updated']))
-							.slice(0, 3) as { asset }, i}
-							<li key={asset.id}>
-								<a href={asset.path} class="rounded-lg block hover:bg-gray-50">
-									<div class="flex items-center min-w-0 flex-1 justify-between">
-										<div class="flex items-center">
-											<div class="text-gray-600 text-sm">
-												{i + 1}
-											</div>
-											<div class="flex items-center px-4 sm:px-6">
-												<div class="font-semibold">
-													{asset.name}
-												</div>
-												<div class="pl-1 text-gray-600">
-													{asset.symbol}
-												</div>
-											</div>
-										</div>
-										<div class="flex items-center">
-											<div>
-												{formatCurrency(asset.backing['backing-usd'])}
-											</div>
-											<div class="ml-5 flex-shrink-0">
-												<ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-											</div>
-										</div>
-									</div>
-								</a>
-							</li>
-						{/each}
-					</ul>
-				</div>
+				<AssetList
+					{assets}
+					icon={ChartBarIcon}
+					title="Backing"
+					compare={(a, b) => b.asset.backing['backing-usd'] - a.asset.backing['backing-usd']}
+					size={3}
+					select={(asset) => formatCurrency(asset.backing['backing-usd'])}
+				/>
+				<AssetList
+					{assets}
+					icon={HashtagIcon}
+					title="Backing Assets"
+					compare={(a, b) => b.asset.backing['backing-assets'] - a.asset.backing['backing-assets']}
+					size={3}
+					select={(asset) => asset.backing['backing-assets']}
+				/>
+				<AssetList
+					{assets}
+					icon={ClockIcon}
+					title="Recently Updated"
+					compare={(a, b) =>
+						new Date(a.asset.backing['updated']) - new Date(b.asset.backing['updated'])}
+					size={3}
+					select={(asset) => formatCurrency(asset.backing['backing-usd'])}
+				/>
 			</dl>
 		</div>
 	</main>
