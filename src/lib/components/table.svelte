67cabb9d7705538e72935bf21b1775ddf264084a
Sebastian Penhouet <s.penhouet@outlook.de>
2022-07-03 01:40:57 +0200

    Use cursor pointer for table header

--- src/lib/components/table.svelte
+++ src/lib/components/table.svelte
@@ -24,7 +24,7 @@
 					{#each columns as column}
 						<th
 							scope="col"
-							class="group py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 {column.class}"
+							class="group cursor-pointer py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 {column.class}"
 							on:click={(e) => {
 								if (sortBy == column.id) {
 									sortAsc = !sortAsc;
