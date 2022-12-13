6a29fa893a2b437afe309eeac9bfd585be6f8085
Sebastian Penhouet <s.penhouet@outlook.de>
2022-12-13 21:32:10 +0100

    Change header level to improve seo

--- src/lib/components/section-header.svelte
+++ src/lib/components/section-header.svelte
@@ -13,11 +13,11 @@
 </script>
 
 <div class="max-w-xl xl:max-w-4xl mx-auto lg:mx-0 px-2 sm:px-0 {clazz}">
-	<h2
+	<h1
 		class="text-5xl sm:text-6xl lg:text-7xl tracking-tight font-extrabold transition-all duration-500 {theme.title}"
 	>
 		{title}
-	</h2>
+	</h1>
 	<p class="mt-5 text-xl sm:text-2xl lg:text-3xl transition-all duration-500">
 		{description}
 	</p>
