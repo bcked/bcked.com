aa2e8d1844077cea81261eeb227bddb57c40cea1
Sebastian Penhouet <s.penhouet@outlook.de>
2022-12-14 22:33:49 +0100

    Add section fly in animation

--- src/lib/components/section.svelte
+++ src/lib/components/section.svelte
@@ -14,6 +14,12 @@
 	/** @type {boolean} */
 	export let isInView = false;
 
+	/**
+	 * When to animate showing the section.
+	 * @type {boolean}
+	 * */
+	export let show = false;
+
 	export let inViewOptions = { threshold: 0.7 };
 
 	function setUrl() {
@@ -21,11 +27,20 @@
 	}
 </script>
 
-<div class="odd:bg-gray-200 odd:text-gray-900 even:bg-neon-gray-light even:text-gray-50">
+<div
+	class="odd:bg-gray-200 odd:text-gray-900 even:bg-neon-gray-light even:text-gray-50"
+	use:inview
+	on:change={(event) => {
+		const { inView, entry, scrollDirection, observer, node } = event.detail;
+		show = inView;
+	}}
+>
 	<section
 		{id}
 		aria-label={label}
-		class="relative text-center lg:text-left max-w-7xl mx-auto py-16 sm:py-24 sm:px-6 lg:px-8 {clazz}"
+		class="relative text-center lg:text-left max-w-7xl mx-auto py-16 sm:py-24 sm:px-6 lg:px-8 {!show
+			? '-translate-x-12 opacity-0 blur-sm'
+			: 'translate-x-0 opacity-100 blur-none'} transition-all duration-1000 {clazz}"
 		use:inview={inViewOptions}
 		on:enter={(event) => {
 			const { inView, entry, scrollDirection, observer, node } = event.detail;
