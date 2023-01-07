22d36af0a52a1fb042fd4e732957b31467354b31
Sebastian Penhouet <s.penhouet@outlook.de>
2023-01-07 17:38:40 +0100

    Add preview screenshots generation

---
+++ tests/preview-screenshots.spec.ts
@@ -0,0 +1,51 @@
+import { test, type Page, type Locator } from '@playwright/test';
+
+import fs from 'fs';
+import { parse } from 'yaml';
+
+const OPEN_GRAPH_IMAGE_SIZE = { width: 1200, height: 830 };
+
+const DEFAULT_BOUNDING_BOX = { x: 0, y: 0, ...OPEN_GRAPH_IMAGE_SIZE };
+
+async function screenshot(page: Page, url: string, path: string, locator?: Locator) {
+	await page.setViewportSize(OPEN_GRAPH_IMAGE_SIZE);
+	// Deactivate fly in animation
+	await page.emulateMedia({ reducedMotion: 'reduce' });
+	await page.goto(url);
+
+	// Calculate your clip rect. For a single element, that's usually the same as it's bounding box.
+	const clipRelativeToViewport = locator
+		? (await locator.boundingBox()) ?? DEFAULT_BOUNDING_BOX
+		: DEFAULT_BOUNDING_BOX;
+
+	// Translate clip to be relative to the page.
+	const clipRelativeToPage = {
+		x: clipRelativeToViewport.x + (await page.evaluate(() => window.scrollX)),
+		y: clipRelativeToViewport.y + (await page.evaluate(() => window.scrollY)),
+		...OPEN_GRAPH_IMAGE_SIZE
+	};
+
+	// Take an area screenshot.
+	await page.screenshot({
+		clip: clipRelativeToPage,
+		fullPage: true,
+		path,
+		quality: 100
+	});
+}
+
+test('Screenshot of landing page', async ({ page }) => {
+	await screenshot(page, '/', './static/preview/landing.jpg', page.locator('nav + div').nth(0));
+});
+
+test('Screenshots of asset pages', async ({ page }) => {
+	const assets = parse(fs.readFileSync(`./_generated/assets.yml`, 'utf-8'));
+	for (const id of Object.keys(assets)) {
+		await screenshot(
+			page,
+			`/assets/${id}`,
+			`./static/preview/assets/${id}.jpg`,
+			page.locator('nav + div').nth(0)
+		);
+	}
+});
