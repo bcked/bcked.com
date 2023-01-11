42cee770db9e203f64ac1f9343c3a074afa6d627
Spenhouet <Spenhouet@users.noreply.github.com>
2023-01-11 21:44:28 +0000

    Deploying to gh-pages from @ bcked/bcked.com@4f41bdc4461cf95dbf32ebd0ca80c553c7434e41 🚀

---
+++ _app/immutable/chunks/4-5496d67c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-62a13c4f.js";const e=!0;export{t as component,e as server};
--- _app/immutable/chunks/5-35de07d3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-a80f768e.js";const e=!0;export{t as component,e as server};
