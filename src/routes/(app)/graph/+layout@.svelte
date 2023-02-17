eb5a066a67a27b868456c6b26e32f88fbe4b3771
Sebastian Penhouet <s.penhouet@outlook.de>
2023-02-17 22:45:32 +0100

    Clean up graph view

--- src/routes/(app)/graph/+layout@.svelte
+++ src/routes/(app)/graph/+layout@.svelte
@@ -2,7 +2,7 @@
 	import { base } from '$app/paths';
 </script>
 
-<div class="absolute z-10 mx-auto px-4 sm:px-6 lg:px-8 flex justify-between h-16">
+<div class="absolute z-20 mx-auto px-4 sm:px-6 lg:px-8 flex justify-between h-16">
 	<div class="flex-shrink-0 flex items-center">
 		<a href={`${base}/`}>
 			<img class="block lg:hidden h-8 w-8" src="{base}/bcked-logo.svg" alt="bcked" />
--- src/routes/(app)/graph/+page.svelte
+++ src/routes/(app)/graph/+page.svelte
@@ -2,7 +2,9 @@
 	/**
 	 * Documentation on 3D Force Graph: https://github.com/vasturiano/3d-force-graph
 	 */
+	import { base } from '$app/paths';
 	import { ForceNGraph3D } from '$lib/utils/graph';
+	import { formatCurrency } from '$lib/utils/string-formatting';
 	import type { ForceGraph3DInstance } from '3d-force-graph';
 	import { onMount } from 'svelte';
 	import * as Three from 'three';
@@ -12,17 +14,10 @@
 
 	export let data: PageData;
 
-	$: ({
-		assetsDetails,
-		assetsContracts,
-		assetsPrice,
-		assetsSupply,
-		assetsBacking,
-		chainsDetails,
-		issuersDetails,
-		icons,
-		graphData
-	} = data);
+	type Node = agg.GraphNode & { x: number; y: number; z: number };
+	type Link = agg.GraphLink;
+
+	$: ({ assetsDetails, assetsContracts, chainsDetails, issuersDetails, icons, graphData } = data);
 
 	let htmlElement: HTMLDivElement;
 
@@ -30,51 +25,66 @@
 	let innerHeight = 0;
 	let graph: ForceGraph3DInstance;
 
+	let selectedNode: string | undefined = undefined;
+	$: selectedName = selectedNode ? assetsDetails[selectedNode]!.name : undefined;
+	$: selectedIssuer = selectedNode
+		? issuersDetails[assetsDetails[selectedNode]?.issuer ?? '']?.name
+		: undefined;
+	$: selectedChain = selectedNode
+		? chainsDetails[assetsContracts[selectedNode]?.token?.chain ?? '']?.name
+		: undefined;
+
 	onMount(() => {
 		graph = ForceNGraph3D(htmlElement)
-			.nodeLabel((node) => {
-				const name = assetsDetails[(node as agg.GraphNode).id]!.name;
-				const issuer =
-					issuersDetails[assetsDetails[(node as agg.GraphNode).id]?.issuer ?? '']?.name;
-				const chain =
-					chainsDetails[assetsContracts[(node as agg.GraphNode).id]?.token?.chain ?? '']?.name;
+			.enableNodeDrag(false)
+			.nodeLabel((n) => {
+				const node = n as Node;
+				const name = assetsDetails[node.id]!.name;
+				const issuer = issuersDetails[assetsDetails[node.id]?.issuer ?? '']?.name;
+				const chain = chainsDetails[assetsContracts[node.id]?.token?.chain ?? '']?.name;
 
 				if (name && issuer && chain) return `${name} - ${issuer} (${chain})`;
 				if (name && issuer) return `${name} - ${issuer}`;
 				if (name && chain) return `${name} (${chain})`;
 				return name;
 			})
-			.nodeThreeObject((node) => {
-				const href = icons[(node as agg.GraphNode).id]!.href;
+			.nodeThreeObject((n) => {
+				const node = n as Node;
+				const href = icons[node.id]!.href;
 				const imgTexture = new Three.TextureLoader().load(href);
 				const material = new Three.SpriteMaterial({ map: imgTexture });
 				const sprite = new Three.Sprite(material);
 				sprite.scale.set(12, 12, 12);
 				return sprite;
 			})
-			.enableNodeDrag(false)
-			.onNodeClick((node) => {
+			.onNodeClick((n) => {
+				const node = n as Node;
+				selectedNode = node.id;
 				// Aim at node from outside it
 				const distance = 150;
 				const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 
-				const newPos =
-					node.x || node.y || node.z
-						? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
-						: { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+				let newPos = { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+				if (node.x && node.y && node.z) {
+					newPos = { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio };
+				}
 
 				graph.cameraPosition(
 					newPos, // new position
-					node, // lookAt ({ x, y, z })
-					2000 // ms transition duration
+					node, // lookAt
+					1000 // ms transition duration
 				);
+			})
+			.linkLabel((l) => {
+				const link = l as Link;
+				return formatCurrency(link.data.backing);
 			})
 			.linkDirectionalArrowLength(3.5)
 			.linkDirectionalArrowRelPos(1)
-			.linkCurvature((link) =>
-				(link as agg.GraphLink).fromId == (link as agg.GraphLink).toId ? 0.75 : 0
-			)
-			// .linkCurvature((link) => 0.0)
+			.linkCurvature((l) => {
+				const link = l as Link;
+				return link.fromId == link.toId ? 0.75 : 0;
+			})
 			.graphData(graphData);
 
 		const resolution = new Vector2(innerWidth, innerHeight);
@@ -90,5 +100,30 @@
 </script>
 
 <svelte:window bind:innerWidth bind:innerHeight />
+
+<div
+	class="flex fixed w-full z-10 px-2 sm:px-4 items-start justify-end transition-all motion-reduce:transition-none duration-1000"
+>
+	<div
+		class="w-full max-w-[15rem] sm:max-w-lg lg:max-w-xl bg-gray-50 shadow rounded-b-lg {!selectedNode
+			? '-translate-y-16 opacity-0'
+			: 'translate-y-0 opacity-100'} transition-all motion-reduce:transition-none duration-1000"
+	>
+		<div class="px-4 py-2 sm:px-6 transition-all motion-reduce:transition-none duration-1000">
+			<h2 class="text-lg font-medium leading-6 text-neon-pink">
+				<a href="{base}/assets/{selectedNode}">{selectedName}</a>
+			</h2>
+			{#if selectedIssuer && selectedChain}
+				<p class="mt-1 max-w-2xl text-sm text-gray-500">
+					Issuer: {selectedIssuer} | Chain: {selectedChain}
+				</p>
+			{:else if selectedIssuer}
+				<p class="mt-1 max-w-2xl text-sm text-gray-500">Issuer: {selectedIssuer}</p>
+			{:else if selectedChain}
+				<p class="mt-1 max-w-2xl text-sm text-gray-500">Chain: {selectedChain}</p>
+			{/if}
+		</div>
+	</div>
+</div>
 
 <div id="3d-graph" bind:this={htmlElement} />
