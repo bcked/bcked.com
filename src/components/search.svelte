4f54a0d57b25dfa6d9e9091b895607022438ee0f
Sebastian Penhouet <s.penhouet@outlook.de>
2023-02-18 19:11:01 +0100

    Fix search box overflow

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -89,12 +89,16 @@
 </script>
 
 <Listbox value={selected}>
-	<div class="relative" use:clickOutside on:click_outside={clearQuery}>
+	<div
+		class="relative whitespace-nowrap overflow-hidden text-ellipsis"
+		use:clickOutside
+		on:click_outside={clearQuery}
+	>
 		<label for="search" class="sr-only">Search</label>
 		<input
 			id="search"
 			name="search"
-			class="block w-full pl-10 pr-3 py-2 rounded-md leading-5 bg-gray-50 focus:bg-white caret-gray-400 focus:outline-none focus:ring-1 focus:ring-transparent border border-gray-300 focus:border-gray-400 sm:text-sm {clazz}"
+			class="block w-full pl-10 pr-3 py-2 rounded-md leading-5 bg-gray-50/80 focus:bg-white/80 caret-gray-400 focus:outline-none focus:ring-1 focus:ring-transparent border border-gray-300 focus:border-gray-400 sm:text-sm {clazz}"
 			type="search"
 			autoComplete="off"
 			bind:value={query}
