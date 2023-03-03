02478f637e11f265680753de8da4c76f1246603b
Sebastian Penhouet <s.penhouet@outlook.de>
2023-03-03 23:12:12 +0100

    Start clean ups

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,112 +1,137 @@
+import { aggregateFolders } from '$lib/utils/aggregation';
 import { readAggregation, writeAggregation, writeJson } from '$lib/utils/files';
-import { readGraph } from '$lib/utils/graph';
-import { loadAssets, loadHistoricalData } from '$pre/assets';
+import { createFromAggregations, writeGraph } from '$lib/utils/graph';
+import { loadHistoricalData } from '$pre/assets';
+import { queryIcons } from '$pre/copy-icons';
 import { queryAssets } from '$pre/query';
-import { writeTimestampFile } from '$pre/record-file';
 import { calcAssetsStats, calcGlobalStats } from '$pre/stats';
 import fs from 'fs';
 
 console.log(`Hooks loading.`);
 
-async function updateData() {
-	// TODO update to account for maps?
-
-	console.log(`Data update started.`);
-	// Update Data
-	const assets = await loadAssets({});
-	const queryResults = await queryAssets(assets);
-
-	for (const [id, data] of Object.entries(queryResults)) {
-		writeTimestampFile(id, 'price', data.price);
-		writeTimestampFile(id, 'supply', data.supply);
-		writeTimestampFile(id, 'backing', data.backing);
-	}
-	console.log(`Data update finished.`);
-}
-
 async function migrateHistoryData(assets: agg.AssetsDetails, t: string) {
+	// TODO remove after migration
 	for (const asset of Object.values(assets)) {
 		const path = `./assets/${asset.id}/${t}`;
 		const history = await loadHistoricalData(path);
 		if (!history) continue;
 		writeJson(path + '.json', { history });
+		// TODO readd
 		// fs.rmSync(path, { recursive: true, force: true });
 	}
 }
 
-async function preprocessData() {
+async function migrate() {
+	// TODO remove after migration
+	// const issuersDetails = await readAggregation<agg.IssuersDetails>('issuers-details');
+	// const chainsDetails = await readAggregation<agg.ChainsDetails>('chains-details');
+	const assetsDetails = await readAggregation<agg.AssetsDetails>('assets-details');
+	// const assetsContracts = await readAggregation<agg.AssetsContracts>('assets-contracts');
+	// const icons = await readAggregation<agg.Icons>('icons');
+
+	// const assetsPrice = await readAggregation<agg.AssetsPrice>('assets-price');
+	// const assetsSupply = await readAggregation<agg.AssetsSupply>('assets-supply');
+	// const assetsBacking = await readAggregation<agg.AssetsBacking>('assets-backing');
+
+	// const graph = readGraph<graph.NodeData, graph.LinkData>('graph');
+
+	// const assetsStats = await readAggregation<agg.AssetsStats>('assets-stats');
+	// const globalStats = await readAggregation<agg.GlobalStats>('global-stats');
+
+	await migrateHistoryData(assetsDetails, 'price');
+	await migrateHistoryData(assetsDetails, 'supply');
+	await migrateHistoryData(assetsDetails, 'backing');
+}
+
+async function aggregateData() {
 	console.log(`Data preprocessing started.`);
 	// Copy static files
-	// const icons = queryIcons('./assets/**/icon.png', 'assets');
-	// writeAggregation('icons', icons);
+	const icons = queryIcons('./assets/**/icon.png', 'assets');
+	writeAggregation('icons', icons);
 
 	// Basic data aggregation
-	// const issuersDetails = await aggregateFolders<derived.IssuerId, agg.IssuerDetails>(
-	// 	'issuers',
-	// 	'details'
-	// );
-	// const chainsDetails = await aggregateFolders<derived.ChainId, agg.ChainDetails>(
-	// 	'chains',
-	// 	'details'
-	// );
-	// const assetsDetails = await aggregateFolders<derived.AssetId, agg.AssetDetails>(
-	// 	'assets',
-	// 	'details'
-	// );
-	// const assetsContracts = await aggregateFolders<derived.AssetId, agg.AssetContracts>(
-	// 	'assets',
-	// 	'contracts'
-	// );
+	const issuersDetails = await aggregateFolders<derived.IssuerId, agg.IssuerDetails>(
+		'issuers',
+		'details'
+	);
+	const chainsDetails = await aggregateFolders<derived.ChainId, agg.ChainDetails>(
+		'chains',
+		'details'
+	);
+	const assetsDetails = await aggregateFolders<derived.AssetId, agg.AssetDetails>(
+		'assets',
+		'details'
+	);
+	const assetsContracts = await aggregateFolders<derived.AssetId, agg.AssetContracts>(
+		'assets',
+		'contracts'
+	);
 
-	// TODO remove temp code
-	const issuersDetails = await readAggregation<agg.IssuersDetails>('issuers-details');
-	const chainsDetails = await readAggregation<agg.ChainsDetails>('chains-details');
-	const assetsDetails = await readAggregation<agg.AssetsDetails>('assets-details');
-	const assetsContracts = await readAggregation<agg.AssetsContracts>('assets-contracts');
-	const icons = await readAggregation<agg.Icons>('icons');
+	let assetsPrice = await aggregateFolders<derived.AssetId, agg.AssetPriceData>('assets', 'price');
+	let assetsSupply = await aggregateFolders<derived.AssetId, agg.AssetSupplyData>(
+		'assets',
+		'supply'
+	);
+	let assetsBacking = await aggregateFolders<derived.AssetId, agg.AssetBackingData>(
+		'assets',
+		'backing'
+	);
 
-	// migrateHistoryData(assetsDetails, 'price');
-	// migrateHistoryData(assetsDetails, 'supply');
-	// migrateHistoryData(assetsDetails, 'backing');
+	await updateData(assetsDetails, assetsPrice, assetsSupply, assetsBacking);
 
-	// const assetsPrice = await aggregateFolders<derived.AssetId, agg.AssetPriceData>(
-	// 	'assets',
-	// 	'price'
-	// );
-	// const assetsSupply = await aggregateFolders<derived.AssetId, agg.AssetSupplyData>(
-	// 	'assets',
-	// 	'supply'
-	// );
-	// const assetsBacking = await aggregateFolders<derived.AssetId, agg.AssetBackingData>(
-	// 	'assets',
-	// 	'backing'
-	// );
-
-	// TODO remove temp code
-	const assetsPrice = await readAggregation<agg.AssetsPrice>('assets-price');
-	const assetsSupply = await readAggregation<agg.AssetsSupply>('assets-supply');
-	const assetsBacking = await readAggregation<agg.AssetsBacking>('assets-backing');
-
-	// const graph = createFromAggregations(
-	// 	assetsDetails,
-	// 	issuersDetails,
-	// 	chainsDetails,
-	// 	icons,
-	// 	assetsContracts,
-	// 	assetsPrice,
-	// 	assetsSupply,
-	// 	assetsBacking
-	// );
-	// writeGraph('graph', graph);
-
-	const graph = readGraph<graph.NodeData, graph.LinkData>('graph');
-
-	// TODO this was just a PoC:
-	// const subgraph = getSubGraph(graph, 'pad-toad.network-moonbeam');
-	// console.log(JSON.stringify(JSON.parse(toJson(subgraph)), null, 4));
+	const graph = createFromAggregations(
+		assetsDetails,
+		issuersDetails,
+		chainsDetails,
+		icons,
+		assetsContracts,
+		assetsPrice,
+		assetsSupply,
+		assetsBacking
+	);
+	writeGraph('graph', graph);
 
 	const assetsStats = calcAssetsStats(graph);
 	const globalStats = calcGlobalStats(graph, assetsStats);
+
+	return {
+		assetsDetails,
+		assetsContracts,
+		assetsPrice,
+		assetsSupply,
+		assetsBacking,
+		assetsStats,
+		chainsDetails,
+		issuersDetails,
+		icons,
+		globalStats,
+		graph
+	};
+}
+
+async function updateData(
+	assetsDetails: agg.AssetsDetails,
+	assetsPrice: agg.AssetsPrice,
+	assetsSupply: agg.AssetsSupply,
+	assetsBacking: agg.AssetsBacking
+) {
+	console.log(`Data update started.`);
+	// Update Data
+	// TODO fix
+	const queryResults = await queryAssets(assets);
+
+	for (const [id, data] of Object.entries(queryResults)) {
+		// TODO Fix typing
+		// TODO fix timestamp as strings not numbers
+		if (data.price) assetsPrice[id]?.history?.push(data.price);
+		if (data.supply) assetsSupply[id]?.history?.push(data.supply);
+		if (data.backing) assetsBacking[id]?.history?.push(data.backing);
+		// TODO replace / reimplement writeTimestampFile to save updated history
+		// writeTimestampFile(id, 'price', data.price);
+		// writeTimestampFile(id, 'supply', data.supply);
+		// writeTimestampFile(id, 'backing', data.backing);
+	}
+	console.log(`Data update finished.`);
 }
 
 const update = await readAggregation<agg.Update>('update');
@@ -115,9 +140,21 @@ if (!update || Date.now() - new Date(update.timestamp).getTime() > 60 * 1000) {
 	console.log(`Preprocessing execution`);
 	fs.mkdirSync('./.cache', { recursive: true });
 
-	// await updateData();
+	// await migrate()
 
-	// await preprocessData();
+	// const {
+	// 	assetsDetails,
+	// 	assetsContracts,
+	// 	assetsPrice,
+	// 	assetsSupply,
+	// 	assetsBacking,
+	// 	assetsStats,
+	// 	chainsDetails,
+	// 	issuersDetails,
+	// 	icons,
+	// 	globalStats,
+	// 	graph
+	// } = await aggregateData();
 
 	// throw new Error('Not implemented.');
 
